2e98f3bba17597193ecca81b4cbe08bc95e17d97
wontgruber <nwilgruber@gmail.com>
2015-08-27 17:01:18 -0400

    Add name and role to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Nick Wilgruber");
+ $("#main").append(funThoughts);
+
+
+
+ var awesomeThoughts = "I am Nick and I am AWESOME!";
+
+ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+/* Okay. Here I'm replacing %data% place holders in helper.js with my 
+actual data */
+var name = "Nick Wilgruber"
+var role = "Geographer"
+
+var formattedName = HTMLheaderName.replace("%data%", name)
+
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").append(formattedRole);
